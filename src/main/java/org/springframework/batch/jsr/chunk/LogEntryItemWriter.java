28573d556a265313af9df3da9859d44916d85a98
Publio Estupiñán <publinchi@gmail.com>
2019-03-21 21:15:43 -0500

    - Se cambia PrepareStatement por JdbcBatchItemWriter

--- src/main/java/org/springframework/batch/jsr/chunk/LogEntryItemWriter.java
+++ src/main/java/org/springframework/batch/jsr/chunk/LogEntryItemWriter.java
@@ -1,52 +1,37 @@
 package org.springframework.batch.jsr.chunk;
 
 import java.io.Serializable;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
 import java.util.List;
 
 import javax.batch.api.chunk.ItemWriter;
 import javax.sql.DataSource;
 
-import org.springframework.batch.jsr.domain.LogEntry;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class LogEntryItemWriter implements ItemWriter {
 
 	@Autowired
 	public DataSource dataSource;
-	private Connection connection;
-	private PreparedStatement statement;
+	private JdbcBatchItemWriter jdbcBatchItemWriter;
 
 	@Override
 	public void open(Serializable checkpoint) throws Exception {
-		connection = dataSource.getConnection();
-		statement = connection.prepareStatement("INSERT INTO logEntry (ip_address, requested_url, country_code, view_date) VALUES(?, ?, ?, ?)");
+		jdbcBatchItemWriter = new JdbcBatchItemWriter();
+		jdbcBatchItemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider());
+		jdbcBatchItemWriter.setSql("INSERT INTO logEntry (ip_address, requested_url, country_code, view_date) VALUES(:ipAddress, :requestedUrl, :countryCode, :viewDate)");
+		jdbcBatchItemWriter.setDataSource(dataSource);
+		jdbcBatchItemWriter.afterPropertiesSet();
 	}
 
 	@Override
 	public void close() throws Exception {
-		statement.close();
-		connection.close();
 	}
 
 	@Override
 	public void writeItems(List<Object> items) throws Exception {
-		if(items != null && !items.isEmpty()) {
-			for (Object item : items) {
-				LogEntry entry = (LogEntry) item;
-
-				statement.setString(1, entry.getIpAddress());
-				statement.setString(2, entry.getRequestedUrl());
-				statement.setString(3, entry.getCountryCode());
-				statement.setDate(4, new Date(entry.getViewDate().getTime()));
-
-				statement.addBatch();
-			}
-
-			statement.executeBatch();
-		}
+		jdbcBatchItemWriter.write(items);
 	}
 
 	@Override
